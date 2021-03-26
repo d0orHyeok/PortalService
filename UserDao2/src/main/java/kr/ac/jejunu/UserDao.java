30d8a94c2a32d926cf5b20e9ba17eab98eb3ee24
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-03-26 17:47:33 +0900

    strategy pattern
    strategy pattern

--- UserDao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ UserDao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -2,9 +2,15 @@ package kr.ac.jejunu;
 
 import java.sql.*;
 
-public abstract class UserDao {
+public class UserDao {
+    private final ConnectionMaker connectionMaker;
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "select * from  userinfo where id = ?"
         );
@@ -22,7 +28,7 @@ public abstract class UserDao {
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = getConnection();
+        Connection connection = connectionMaker.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "insert into userinfo (name, password) values (? , ?)"
                 , Statement.RETURN_GENERATED_KEYS
@@ -40,13 +46,4 @@ public abstract class UserDao {
         preparedStatement.close();
         connection.close();
     }
-
-    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
-//    {
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        return DriverManager.getConnection(
-//                "jdbc:mysql://localhost/portal?characterEncoding=utf-8&serverTimezone=UTC"
-//                , "jeju", "jejupw"
-//        );
-//    }
 }
