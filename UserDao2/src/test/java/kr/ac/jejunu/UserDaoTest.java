bd61ce4f5c08d37b13237185b1b9ec7c8de5a43f
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-03-26 17:24:11 +0900

    Create UserDao2
    UserDao 문제로 UserDao2 생성

---
+++ UserDao2/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -0,0 +1,75 @@
+package kr.ac.jejunu;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+
+public class UserDaoTest {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        Integer id = 1;
+        String name = "hyeok";
+        String password = "1234";
+
+        UserDao userDao = new JejuUserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "kjh";
+        String password = "1111";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new JejuUserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+
+    @Test
+    public void getHalla() throws SQLException, ClassNotFoundException {
+        Integer id = 1;
+        String name = "hyeok";
+        String password = "1234";
+
+        UserDao userDao = new HallaUserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insertHalla() throws SQLException, ClassNotFoundException {
+        String name = "kjh";
+        String password = "1111";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new HallaUserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+}
