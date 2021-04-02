28fa4a7c19a070c439eb1a0d07580ae82ae35bc6
KimJangHyeok <48301140+d0orHyeok@users.noreply.github.com>
2021-04-02 17:42:02 +0900

    exception

--- UserDao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ UserDao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -13,41 +13,72 @@ public class UserDao {
     public User findById(Integer id) throws SQLException {
         // 변하는 것 stretagy
         // 변하는 것은 interface로 선언하여 뽑아내기
-        Connection connection = dataSource.getConnection();
         // 변하지 않는 것 context
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "select * from  userinfo where id = ?"
-        );
-        preparedStatement.setInt(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "select * from  userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
         return user;
     }
 
     public void insert(User user) throws SQLException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into userinfo (name, password) values (? , ?)"
-                , Statement.RETURN_GENERATED_KEYS
-        );
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "insert into userinfo (name, password) values (? , ?)"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
 
-        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
 
-        user.setId(resultSet.getInt(1));
-
-        preparedStatement.close();
-        connection.close();
+            user.setId(resultSet.getInt(1));
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
     }
 }
